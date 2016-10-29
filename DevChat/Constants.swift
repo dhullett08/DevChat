7ebb7ff1fcabab692fb3208c4382925f00a5a616
Dustin Hullett <dustin.hullett@arc-cpcn.com>
2016-10-29 16:38:50 -0500

    made rounded UI Buttons

---
+++ DevChat/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  DevChat
+//
+//  Created by Dustin Hullett on 10/29/16.
+//  Copyright © 2016 Dustin Hullett. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.5
